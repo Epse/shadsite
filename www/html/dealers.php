62f491b2866868c37ca91ac1c94325f2c6160163
Epse <stef.plet@gmail.com>
2015-08-01 19:44:11 +0200

    first test of include system

--- www/html/dealers.php
+++ www/html/dealers.php
@@ -4,7 +4,7 @@
 <head>
 <title>Dealers</title>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<meta name="Keywords" content="GSX-R world contest wereldrecord 2010 Haùmme-Zogge Suzuki hayabusa Belgium">
+<meta name="Keywords" content="GSX-R world contest wereldrecord 2010 Haï¿½mme-Zogge Suzuki hayabusa Belgium">
 <meta name="Generator" content="NetObjects Fusion 10 for Windows">
 <script type="text/javascript">
 <!--
@@ -17,56 +17,15 @@ function F_loadRollover(){} function F_roll(){}
 </head>
 <body style="text-align: center; background-color: rgb(0,0,0); margin: 0px;">
  <div align="center">
- <table border="0" cellspacing="0" cellpadding="0">
-  <tr>
-   <td>
-    <table border="0" cellspacing="0" cellpadding="0">
-     <tr valign="top" align="left">
-      <td width="960">
-       <table id="Table49" border="0" cellspacing="0" cellpadding="0" width="100%" style="border-left-color:  rgb(255,255,255); border-left-style: solid; border-top-color:  rgb(255,255,255); border-top-style: solid; border-right-color:  rgb(255,255,255); border-right-style: solid; border-bottom-color:  rgb(255,255,255); border-bottom-style: solid; height: 193;">
-        <tr style="height: 18px;">
-         <td width="960" style="background-color: rgb(0,0,0); padding: 2px;">
-          <p style="text-align: center;"><b><span style="font-size: 12px; color: rgb(255,255,255);">SHAD Belgium : cases - seats - lugage : <a href="mailto:info@shad.be" target="_blank">info@shad.be</a></span></b></p>
-         </td>
-        </tr>
-        <tr style="height: 155px;">
-         <td style="background-color: rgb(255,255,255);">
-          <p><a href="../index.html"><img id="Picture6" height="155" width="960" src="../assets/images/topbanner_bikeparts_990_175_SHAD.jpg" vspace="0" hspace="0" align="TOP" border="0" alt="SHAD BENELUX" title="SHAD BENELUX" usemap="#map0"><map name="map0"><area shape="rect" alt="" coords="827,1,960,121" href="http://www.shad.es/getCatalogue.php"><area shape="rect" alt="SHAD BENELUX" title="SHAD BENELUX" coords="0,0,959,154" href="../index.html"></map></a></p>
-         </td>
-        </tr>
-        <tr style="height: 20px;">
-         <td style="background-image: url('../assets/images/bg1.gif');">
-          <table width="100%" border="0" cellspacing="0" cellpadding="0">
-           <tr>
-            <td align="CENTER">
-             <table id="NavigationBar10" border="0" cellspacing="0" cellpadding="0" width="960">
-              <tr valign="top" align="left">
-               <td width="120"><a href="../index.html" style="cursor:hand; text-decoration:none;" onMouseOver="F_loadRollover('NavigationButton1','',0);F_roll('NavigationButton1',1)" onMouseOut="F_roll('NavigationButton1',0)"><img id="NavigationButton1" name="NavigationButton1" height="20" width="120" src="../assets/images/autogen/Shad_Benelux_Nregular_1_-_kopie_1.gif" onmouseover="F_loadRollover(this,'Shad_Benelux_NRregular_1_-_kopie_1.gif',0)" border="0" alt="Shad Benelux" title="Shad Benelux"></a></td>
-               <td width="120"><a href="../html/welkom.html" style="cursor:hand; text-decoration:none;" onMouseOver="F_loadRollover('NavigationButton2','',0);F_roll('NavigationButton2',1)" onMouseOut="F_roll('NavigationButton2',0)"><img id="NavigationButton2" name="NavigationButton2" height="20" width="120" src="../assets/images/autogen/Welkom_Nregular_1_-_kopie_1.gif" onmouseover="F_loadRollover(this,'Welkom_NRregular_1_-_kopie_1.gif',0)" border="0" alt="Welkom" title="Welkom"></a></td>
-               <td width="120"><a href="../html/bienvenu.html" style="cursor:hand; text-decoration:none;" onMouseOver="F_loadRollover('NavigationButton3','',0);F_roll('NavigationButton3',1)" onMouseOut="F_roll('NavigationButton3',0)"><img id="NavigationButton3" name="NavigationButton3" height="20" width="120" src="../assets/images/autogen/Bienvenu_Nregular_1_-_kopie_1.gif" onmouseover="F_loadRollover(this,'Bienvenu_NRregular_1_-_kopie_1.gif',0)" border="0" alt="Bienvenu" title="Bienvenu"></a></td>
-               <td width="120"><a href="../html/products.html" style="cursor:hand; text-decoration:none;" onMouseOver="F_loadRollover('NavigationButton4','',0);F_roll('NavigationButton4',1)" onMouseOut="F_roll('NavigationButton4',0)"><img id="NavigationButton4" name="NavigationButton4" height="20" width="120" src="../assets/images/autogen/Products_Nregular_1_-_kopie_1.gif" onmouseover="F_loadRollover(this,'Products_NRregular_1_-_kopie_1.gif',0)" border="0" alt="Products" title="Products"></a></td>
-               <td width="120"><a href="../html/dealers.html" style="cursor:hand; text-decoration:none;" onMouseOver="F_loadRollover('NavigationButton5','',0);F_roll('NavigationButton5',1)" onMouseOut="F_roll('NavigationButton5',0)"><img id="NavigationButton5" name="NavigationButton5" height="20" width="120" src="../assets/images/autogen/Dealers_Hregular_2_-_kopie_1.gif" onmouseover="F_loadRollover(this,'Dealers_HRregular_3_1.gif',0)" border="0" alt="Dealers" title="Dealers"></a></td>
-               <td width="120"><a href="../html/play___win__.html" style="cursor:hand; text-decoration:none;" onMouseOver="F_loadRollover('NavigationButton6','',0);F_roll('NavigationButton6',1)" onMouseOut="F_roll('NavigationButton6',0)"><img id="NavigationButton6" name="NavigationButton6" height="20" width="120" src="../assets/images/autogen/Play___Win___Nregular_1_-_kopie_1.gif" onmouseover="F_loadRollover(this,'Play___Win___NRregular_1_-_kopie_1.gif',0)" border="0" alt="Play & Win !" title="Play & Win !"></a></td>
-               <td width="120"><a href="../html/newsletter.html" style="cursor:hand; text-decoration:none;" onMouseOver="F_loadRollover('NavigationButton7','',0);F_roll('NavigationButton7',1)" onMouseOut="F_roll('NavigationButton7',0)"><img id="NavigationButton7" name="NavigationButton7" height="20" width="120" src="../assets/images/autogen/Newsletter_Nregular_1_-_kopie_1.gif" onmouseover="F_loadRollover(this,'Newsletter_NRregular_1_-_kopie_1.gif',0)" border="0" alt="Newsletter" title="Newsletter"></a></td>
-               <td width="120" height="20"><a href="../html/contact__.html" style="cursor:hand; text-decoration:none;" onMouseOver="F_loadRollover('NavigationButton8','',0);F_roll('NavigationButton8',1)" onMouseOut="F_roll('NavigationButton8',0)"><img id="NavigationButton8" name="NavigationButton8" height="20" width="120" src="../assets/images/autogen/Contact___Nregular_1_-_kopie_1.gif" onmouseover="F_loadRollover(this,'Contact___NRregular_1_-_kopie_1.gif',0)" border="0" alt="Contact !" title="Contact !"></a></td>
-              </tr>
-             </table>
-            </td>
-           </tr>
-          </table>
-         </td>
-        </tr>
-       </table>
-      </td>
-     </tr>
-    </table>
+<!-- header include -->
+<?php include '../include/header.html'; ?>
     <table border="0" cellspacing="0" cellpadding="0">
      <tr valign="top" align="left">
       <td width="960">
        <table id="Table53" border="0" cellspacing="0" cellpadding="5" width="100%" style="background-color: rgb(255,255,255); height: 32;">
         <tr valign="top" align="left" style="height: 13px;">
          <td width="229">
-          <p><b>België - Belgique :</b></p>
+          <p><b>Belgiï¿½ - Belgique :</b></p>
          </td>
          <td width="229">
           <p>&nbsp;</p>
@@ -96,7 +55,7 @@ function F_loadRollover(){} function F_roll(){}
          </td>
          <td valign="top">
           <ul>
-           <li><b>Golden Bikes</b><br>Chaussée de Mons 56<br>1430 <b>Quenast</b><br><a href="http://www.goldenbikes.be" target="_blank">www.goldenbikes.be</a><br>tel : 067 33 35 90<br><a href="mailto:info@goldenbikes.be">info@goldenbikes.be</a></li>
+           <li><b>Golden Bikes</b><br>Chaussï¿½e de Mons 56<br>1430 <b>Quenast</b><br><a href="http://www.goldenbikes.be" target="_blank">www.goldenbikes.be</a><br>tel : 067 33 35 90<br><a href="mailto:info@goldenbikes.be">info@goldenbikes.be</a></li>
           </ul>
          </td>
         </tr>
@@ -108,7 +67,7 @@ function F_loadRollover(){} function F_roll(){}
          </td>
          <td>
           <ul>
-           <li><b>Megastar Scoot</b><br>Chaussée de Louvain 501 B<br>1300 <b>Wavre</b><br><a href="http://www.megastarscoot.be" target="_blank">www.megastarscoot.be</a><br>tel : 010 88 85 52<br><a href="mailto:info@megastarscoot.be" target="_blank">info@megastarscoot.be</a></li>
+           <li><b>Megastar Scoot</b><br>Chaussï¿½e de Louvain 501 B<br>1300 <b>Wavre</b><br><a href="http://www.megastarscoot.be" target="_blank">www.megastarscoot.be</a><br>tel : 010 88 85 52<br><a href="mailto:info@megastarscoot.be" target="_blank">info@megastarscoot.be</a></li>
           </ul>
          </td>
          <td>
@@ -130,7 +89,7 @@ function F_loadRollover(){} function F_roll(){}
          </td>
          <td>
           <ul>
-           <li><b>Sud Moto<br></b>Chaussée d&#8217;Alsemberg 414 B<br>1180 BXL Uccle / Ukkel<br>tel : 02 344 00 19<br><a href="mailto:pascalschmitz@skynet.be">pascalschmitz@skynet.be</a></li>
+           <li><b>Sud Moto<br></b>Chaussï¿½e d&#8217;Alsemberg 414 B<br>1180 BXL Uccle / Ukkel<br>tel : 02 344 00 19<br><a href="mailto:pascalschmitz@skynet.be">pascalschmitz@skynet.be</a></li>
           </ul>
          </td>
          <td>
@@ -195,7 +154,7 @@ function F_loadRollover(){} function F_roll(){}
           <p><br><b>Momenteel werken wij volop aan de introductie van nieuwe verkooppunten.</b>&nbsp;<br><br>Regelmatig zal deze pagina aangepast worden. Schrijf U daarom zeker in op onze <span style="color: rgb(255,0,0);">nieuwsbrief</span>. Zo blijft U geinformeerd over de ontwikkelingen&nbsp;rond de SHAD produkten.<br>&nbsp;</p>
          </td>
          <td colspan="2" valign="top">
-          <p><b><br>Actuellement, on s&#8217;organise pour vous offrir des nouveaux points de vente.</b><br><br>Cette page sera mise à jour régulièrement. Inscrivez-vous pour notre <a href="../html/newsletter.html"><span style="color: rgb(255,0,0);">lettre d&#8217;information</span></a>. On vous tiendra au courant sur le développement des produits de SHAD.</p>
+          <p><b><br>Actuellement, on s&#8217;organise pour vous offrir des nouveaux points de vente.</b><br><br>Cette page sera mise ï¿½ jour rï¿½guliï¿½rement. Inscrivez-vous pour notre <a href="../html/newsletter.html"><span style="color: rgb(255,0,0);">lettre d&#8217;information</span></a>. On vous tiendra au courant sur le dï¿½veloppement des produits de SHAD.</p>
          </td>
         </tr>
        </table>
@@ -208,8 +167,8 @@ function F_loadRollover(){} function F_roll(){}
        <table id="Table52" border="0" cellspacing="0" cellpadding="0" width="100%" style="background-color: rgb(255,255,255); border-left-color:  rgb(255,255,255); border-left-style: solid; border-top-color:  rgb(255,255,255); border-top-style: solid; border-right-color:  rgb(255,255,255); border-right-style: solid; border-bottom-color:  rgb(255,255,255); border-bottom-style: solid; height: 278;">
         <tr style="height: 101px;">
          <td width="960">
-          <p style="text-align: center;"><br>Koffers, tassen, zadels en accessoires van superieure kwaliteit.<br>Coffres, sacs, selles et accessoires de qualité supérieur.<br><hr id="HRRule1" width="100%" align="center" size="2"></p>
-          <p style="text-align: center;">Visit SHAD International <b> : </b><a href="http://www.shad.es/en/shad-world/" target="_blank">English</a> - <a href="http://www.shad.es/fr/shad-world/" target="_blank">Français</a> - <a href="http://www.shad.es/de/shad-world/" target="_blank">Deutch</a></p>
+          <p style="text-align: center;"><br>Koffers, tassen, zadels en accessoires van superieure kwaliteit.<br>Coffres, sacs, selles et accessoires de qualitï¿½ supï¿½rieur.<br><hr id="HRRule1" width="100%" align="center" size="2"></p>
+          <p style="text-align: center;">Visit SHAD International <b> : </b><a href="http://www.shad.es/en/shad-world/" target="_blank">English</a> - <a href="http://www.shad.es/fr/shad-world/" target="_blank">Franï¿½ais</a> - <a href="http://www.shad.es/de/shad-world/" target="_blank">Deutch</a></p>
          </td>
         </tr>
         <tr style="height: 312px;">
@@ -233,4 +192,3 @@ function F_loadRollover(){} function F_roll(){}
   </div>
  </body>
  </html>
-  
