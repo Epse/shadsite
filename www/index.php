3017f042951c80243e0256b3d561f52df2f2f632
epse <stef.plet@gmail.com>
2015-08-19 16:23:19 +0200

    fixed CSS references in `index.php`

--- www/index.php
+++ www/index.php
@@ -1,40 +1,38 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
-<head>
-<title>Shad.be</title>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<meta name="Keywords" content="GSX-R world contest wereldrecord 2010 Ha�mme-Zogge Suzuki hayabusa Belgium">
-<meta name="Generator" content="NetObjects Fusion 10 for Windows">
-<title>Bike Parts, garage, motorshop, dealer, Suzuki, Sym, Keeway, Skyteam, service, Honda, Kawasaki, Yamaha, BMW, Vespa, Triumph</title>
-<meta name="description" content="Motoratelier & shop BVBA Bike Parts te Grembergen (Dendermonde). Meer dan een garage : Ontwikkeling onderdelen, testbank, tuning, projectontwikkeling, competitie, depannage & ongevalafhandeling, revisie, kleding, helmen, advies & begeleiding." />
-<meta name="keywords" content="Werkhuizen, motorzaak, motorshop, garage, dealer, Bike Parts, Dendermonde, Grembergen, 9200, depannage, ongevalschade, reparatie, winkel, Suzuki, GSX-R, GSF, B-King, Hayabusa, GSR, Gladius, Intruder, DL V-Strom, Sym, Mio, GTS, Keeway, Skyteam, motoren, scooters, quads, bromfietsen, replica, dax, Skymax, ZH-B 50, monkey, t-rex, PBR, Bike Design, Honda, Gepida, electrische fiets, Yamaha, Kawasaki, BMW, Piaggio, Vespa, Triumph, Belgi�, Belgium, service, praktijkexamen, voorlopig motorrijbewijs, motor, rijbewijs, MAG, screening, testrit, Evolution, bandencentrale, parts, accessoires, onderdelen, koop, bestel, verkoop, 49, cc, 50, 125, 250, 400, 450, 500, 600, 650, 750, 800, 1000, 1250, 1500, 1800, Bikeparts, Bike-Parts, Bikeparts, bikerparts, bikereparts, bikepart, Bike Design, chroom, afvlakken, revisie, motoren, testbank, power Commander, uitlaten, francorchamps, racing, Oost Vlaanderen, St.-Niklaas, Gent, Aalst, Antwerpen, Brussel, Asse, Brabant, Google, Johan Geerinck, Marc, alarminstallatie, advies, hulp, help, cross, quad, buggy, humor, lachen, lol, plezier, ambiance, rock, goesting, biken, ride-out, treffen" />
-<meta name="robots" content="index,follow" />
-<meta name="revisit-after" content="15" />
-<meta name="author" content="Carl" />
-<script type="text/javascript">
-<!--
-function F_loadRollover(){} function F_roll(){}
-//-->
-</script>
-<SCRIPT TYPE="text/javascript" SRC="./assets/rollover.js"></SCRIPT>
-<link rel="stylesheet" type="text/css" href="./html/style.css">
-<link rel="stylesheet" type="text/css" href="./html/site.css">
-</head>
-<body style="text-align: center; background-color: rgb(0,0,0); background-image: none; margin: 0px;">
-  <!-- header include -->
-  <?php include 'include/header.html'; ?>
-  <div class="maincontent">
-    <p>
-      SHAD Benelux verdeelt het volledige aanbod van SHAD internationaal. SHAD is fabrikant van koffers, tassen, zadels en accessoires van superieure kwaliteit.<br><br>
-      Meer informatie in het <a href="./html/welkom.html">Nederlands</a>.
-    </p>
-    <p>
-      SHAD Benelux distribue tout les produits de SHAD international. SHAD est fabricant de coffres, sacs, selles et accessoires de qualit&eacute; sup&eacute;rieure.<br><br>
-      Plus d&#8217;information <a href="./html/bienvenu.html">en fran&ccedil;ais</a>
-    </p>
-    <img id="Picture4" height="200" width="800" src="./assets/images/SHAD_Sportrack_boven___onder_800.png" border="0" alt="SHAD Sportrack boven &amp; onder 800" title="SHAD Sportrack boven &amp; onder 800">
-  </div>
-  <!-- footer include -->
-  <?php include 'include/footer.html'; ?>
- </body>
+  <head>
+    <title>Shad.be</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+    <meta name="Keywords" content="GSX-R world contest wereldrecord 2010 Ha�mme-Zogge Suzuki hayabusa Belgium">
+    <title>Bike Parts, garage, motorshop, dealer, Suzuki, Sym, Keeway, Skyteam, service, Honda, Kawasaki, Yamaha, BMW, Vespa, Triumph</title>
+    <meta name="description" content="Motoratelier &amp; shop BVBA Bike Parts te Grembergen (Dendermonde). Meer dan een garage : Ontwikkeling onderdelen, testbank, tuning, projectontwikkeling, competitie, depannage & ongevalafhandeling, revisie, kleding, helmen, advies & begeleiding." />
+    <meta name="keywords" content="Werkhuizen, motorzaak, motorshop, garage, dealer, Bike Parts, Dendermonde, Grembergen, 9200, depannage, ongevalschade, reparatie, winkel, Suzuki, GSX-R, GSF, B-King, Hayabusa, GSR, Gladius, Intruder, DL V-Strom, Sym, Mio, GTS, Keeway, Skyteam, motoren, scooters, quads, bromfietsen, replica, dax, Skymax, ZH-B 50, monkey, t-rex, PBR, Bike Design, Honda, Gepida, electrische fiets, Yamaha, Kawasaki, BMW, Piaggio, Vespa, Triumph, Belgi�, Belgium, service, praktijkexamen, voorlopig motorrijbewijs, motor, rijbewijs, MAG, screening, testrit, Evolution, bandencentrale, parts, accessoires, onderdelen, koop, bestel, verkoop, 49, cc, 50, 125, 250, 400, 450, 500, 600, 650, 750, 800, 1000, 1250, 1500, 1800, Bikeparts, Bike-Parts, Bikeparts, bikerparts, bikereparts, bikepart, Bike Design, chroom, afvlakken, revisie, motoren, testbank, power Commander, uitlaten, francorchamps, racing, Oost Vlaanderen, St.-Niklaas, Gent, Aalst, Antwerpen, Brussel, Asse, Brabant, Google, Johan Geerinck, Marc, alarminstallatie, advies, hulp, help, cross, quad, buggy, humor, lachen, lol, plezier, ambiance, rock, goesting, biken, ride-out, treffen" />
+    <meta name="robots" content="index,follow" />
+    <meta name="revisit-after" content="15" />
+    <script type="text/javascript">
+      <!--
+      function F_loadRollover(){} function F_roll(){}
+      //-->
+    </script>
+    <SCRIPT TYPE="text/javascript" SRC="./assets/rollover.js"></SCRIPT>
+    <link rel="stylesheet" type="text/css" href="./style.css">
+    <link rel="stylesheet" type="text/css" href="./site.css">
+  </head>
+  <body style="text-align: center; background-color: rgb(0,0,0); background-image: none; margin: 0px;">
+    <!-- header include -->
+    <?php include 'include/header.html'; ?>
+    <div class="maincontent">
+      <p>
+        SHAD Benelux verdeelt het volledige aanbod van SHAD internationaal. SHAD is fabrikant van koffers, tassen, zadels en accessoires van superieure kwaliteit.<br><br>
+        Meer informatie in het <a href="./html/welkom.html">Nederlands</a>.
+      </p>
+      <p>
+        SHAD Benelux distribue tout les produits de SHAD international. SHAD est fabricant de coffres, sacs, selles et accessoires de qualit&eacute; sup&eacute;rieure.<br><br>
+        Plus d&#8217;information <a href="./html/bienvenu.html">en fran&ccedil;ais</a>
+      </p>
+      <img id="Picture4" height="200" width="800" src="./assets/images/SHAD_Sportrack_boven___onder_800.png" border="0" alt="SHAD Sportrack boven &amp; onder 800" title="SHAD Sportrack boven &amp; onder 800">
+    </div>
+    <!-- footer include -->
+    <?php include 'include/footer.html'; ?>
+   </body>
  </html>
