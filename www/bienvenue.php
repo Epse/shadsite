405829242357b2b95f79c8a90723dcc0e3aa1fc8
epse <stef.plet@gmail.com>
2015-08-20 14:24:04 +0200

    starting to fix up bienvenue.php

--- www/bienvenue.php
+++ www/bienvenue.php
@@ -11,20 +11,14 @@
   <body style="text-align: center; background-color: rgb(0,0,0); margin: 0px;">
    <div align="center">
      <!-- header include -->
-     <?php include './include/header.html'; ?>
-      <table border="0" cellspacing="0" cellpadding="0">
-       <tr valign="top" align="left">
-        <td width="960">
-         <table id="Table5" border="0" cellspacing="0" cellpadding="5" width="100%" style="background-color: rgb(255,255,255); height: 32;">
-          <tr style="height: 195px;">
-           <td width="950">
-            <p><img id="Picture2" height="185" width="200" src="./assets/images/shad_sh43_on_bike.jpg" vspace="0" hspace="0" align="RIGHT" border="0" alt="shad sh43 on bike" title="shad sh43 on bike"><br><b>Soyez bienvenu sur le site de Shad et dans le monde technique des produits de bagage.<br></b><br>Considerez nos solutions dans le domaine des topcases, coffres,&nbsp;bagage en confort.<br><br>Etonnez-vous de ce qu&#8217;on peut vous offrir pour vos besoins de transport dans ce monde qui chage rapidement au niveau de la mobilit�. Experiencez les produits de SHAD. la meilleur qualit� avec nombreuses options et applications.<br><br>Inscrivez-vous pour notre <a href="./html/newsletter.html"><span style="color: rgb(255,0,0);">lettre d&#8217;information</span></a> et restez au courant de nos nouveaux points de vente, nouveaux produits, trends et evolution. <br><br><b>Pas trouv� ce que vous cherchez pour votre moto?</b><br>Pas de probl�me. Avec notre exp�rience de longues ann�es dans le fabricage et construction autour des motos, on peut vous offrir une solution. <br>Contactez-nous !<span style="color: rgb(255,0,0);"> <a href="mailto:info@shad.be" CLASS="moz-txt-link-abbreviated">info@shad.be</a></span>.</p>
-           </td>
-          </tr>
-         </table>
-        </td>
-       </tr>
-      </table>
+    <?php include './include/header.html'; ?>
+    <div class="maincontent">
+      <p>
+        Soyez bienvenu sur le site de Shad et dans le monde technique des produits de bagage.<br><br>Considerez nos solutions dans le domaine des topcases, coffres,&nbsp;bagage en confort.
+        <br><br>Etonnez-vous de ce qu&#39;on peut vous offrir pour vos besoins de transport dans ce monde qui chage rapidement au niveau de la mobilit&eacute;. Experiencez les produits de SHAD. la meilleur qualit&eacute; avec nombreuses d&#39;options et applications.<br><br>Inscrivez-vous pour notre <a href="./html/newsletter.html"><span style="color: rgb(255,0,0);">lettre d&#39;information</span></a> et restez au courant de nos nouveaux points de vente, nouveaux produits, trends et evolution. <br><br><b>Pas trouv� ce que vous cherchez pour votre moto?</b><br>Pas de probl�me. Avec notre exp�rience de longues ann�es dans le fabricage et construction autour des motos, on peut vous offrir une solution. <br>Contactez-nous !<span style="color: rgb(255,0,0);"> <a href="mailto:info@shad.be" CLASS="moz-txt-link-abbreviated">info@shad.be</a>
+        <img height="185" width="200" src="./assets/images/shad_sh43_on_bike.jpg" vspace="0" hspace="0" align="RIGHT" border="0" alt="shad sh43 on bike" title="shad sh43 on bike">
+      </p>
+    </div>
       <!-- footer include -->
       <?php include 'include/footer.html'; ?>
     </div>
