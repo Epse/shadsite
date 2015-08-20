c9bed3a2e43c3fa5d419fd677c72dfb3b71132c9
epse <stef.plet@gmail.com>
2015-08-20 14:30:20 +0200

    align that stuffs!

--- www/bienvenue.php
+++ www/bienvenue.php
@@ -13,11 +13,16 @@
      <!-- header include -->
     <?php include './include/header.html'; ?>
     <div class="maincontent">
-      <p class="lefted">
+      <p class="lefted" style="float:left;">
         Soyez bienvenu sur le site de Shad et dans le monde technique des produits de bagage.<br><br>Considerez nos solutions dans le domaine des topcases, coffres,&nbsp;bagage en confort.
-        <br><br>Etonnez-vous de ce qu&#39;on peut vous offrir pour vos besoins de transport dans ce monde qui chage rapidement au niveau de la mobilit&eacute;. Experiencez les produits de SHAD. la meilleur qualit&eacute; avec nombreuses d&#39;options et applications.<br><br>Inscrivez-vous pour notre <a href="./html/newsletter.html"><span style="color: rgb(255,0,0);">lettre d&#39;information</span></a> et restez au courant de nos nouveaux points de vente, nouveaux produits, trends et evolution. <br><br><b>Pas trouv� ce que vous cherchez pour votre moto?</b><br>Pas de probl�me. Avec notre exp�rience de longues ann�es dans le fabricage et construction autour des motos, on peut vous offrir une solution. <br>Contactez-nous !<span style="color: rgb(255,0,0);"> <a href="mailto:info@shad.be" CLASS="moz-txt-link-abbreviated">info@shad.be</a>
-        <img height="185" width="200" src="./assets/images/shad_sh43_on_bike.jpg" vspace="0" hspace="0" align="RIGHT" border="0" alt="shad sh43 on bike" title="shad sh43 on bike">
+        <br><br>Etonnez-vous de ce qu&#39;on peut vous offrir pour vos besoins de transport dans ce monde qui chage rapidement au niveau de la mobilit&eacute;. Experiencez les produits de SHAD. la meilleur qualit&eacute; avec nombreuses d&#39;options et applications.<br><br>
+        Inscrivez-vous pour notre <a href="./html/newsletter.html"><span style="color: rgb(255,0,0);">lettre d&#39;information</span></a> et restez au courant de nos nouveaux points de vente, nouveaux produits, trends et evolution. <br><br>
+        Pas trouv&eacute; ce que vous cherchez pour votre moto?<br>
+        Pas de probl&egrave;me. Avec notre exp&eacute;rience de longues ann&eacute;es dans le fabricage et construction autour des motos, on peut vous offrir une solution.
+        <br>Contactez-nous !<span style="color: rgb(255,0,0);"> <a href="mailto:info@shad.be" CLASS="moz-txt-link-abbreviated">info@shad.be</a>
       </p>
+      <img style="float:left;" height="185" width="200" src="./assets/images/shad_sh43_on_bike.jpg" vspace="0" hspace="0" align="RIGHT" border="0" alt="shad sh43 on bike" title="shad sh43 on bike">
+      <div class="clearer"></div>
     </div>
       <!-- footer include -->
       <?php include 'include/footer.html'; ?>
