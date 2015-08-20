81c45016e145f3bbd097762d51abb0d37e03c101
Stef Pletinck <stef.plet@gmail.com>
2015-08-20 19:09:28 +0200

    fixed txt in `welkom.php`

--- www/welkom.php
+++ www/welkom.php
@@ -10,6 +10,16 @@
 </head>
 <body style="text-align: center; background-color: rgb(0,0,0); margin: 0px;">
  <div align="center">
+   <div class="maincontent">
+     <b>Welkom op de website van Shad&nbsp;en in de technische wereld van de bagageprodukten.</b><br><br>
+     Wandel even door de mogelijkheden op gebied van topcases, koffersets,&nbsp;bagage en comfort.<br><br>
+     U zal versteld staan welke oplossingen wij aanreiken voor Uw&nbsp;transportbehoeften in de snel veranderende mobiliteitsomgeving.
+     <br>Ervaar&nbsp;dat de produkten van SHAD van de beste kwaliteit zijn, met een waaier&nbsp;van toepassingen . <br><br>
+     Schrijf U in op onze <a href="../html/newsletter.html"><span style="color: rgb(255,0,0);">newsletter</span></a> en wordt regelmatig&nbsp;op de hoogte gehouden van nieuwe verkooppunten , de allernieuwste&nbsp;produkten , trends en evoluties. <br><br>
+     <b>Niet gevonden wat U zoekt voor Uw eigen&nbsp;motor? </b><br>
+     Geen probleem, dankzij onze jarenlange ervaring in de fabricage&nbsp;en constructie rond motorfietsen kunnen wij U mogelijks een oplossing&nbsp;aanreiken.
+     <br>Contacteer ons !<span style="color: rgb(255,0,0);"> <a href="mailto:info@shad.be" class="moz-txt-link-abbreviated">info@shad.be</a></span>.
+   </div>
    <!-- header include -->
    <?php include './include/header.html'; ?>
    <!-- footer include -->
