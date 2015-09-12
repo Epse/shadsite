004b3c06e63c4dc60628c5a01cf106a4c8c73a43
Stef Pletinck <stef.plet@gmail.com>
2015-09-12 14:21:20 +0200

    new navbar! again!

--- www/include/header.php
+++ www/include/header.php
@@ -4,20 +4,9 @@
   <map name="headermap">
     <area shape="rect" coords="844,0,990,175" href="http://www.shad.es/getCatalogue.php">
   </map>
-  <ul class="hornavbar">
-    <li class="hornavbar"><a href="./index.php" class="hornavlink">Shad Benelux</a></li>
-    <li class="hornavbar"><?php echo $PHP_SELF;?></li>
-    <li class="hornavbar"><a href="./welkom.php" class="hornavlink">Welkom</a></li>
-    <li class="hornavbar"><a href="./bienvenue.php" class="hornavlink">Bienvenue</a></li>
-    <li class="hornavbar"><a href="./products.php" class="hornavlink">Products</a></li>
-    <li class="hornavbar"><a href="./dealers.php" class="hornavlink">Dealers</a></li>
-    <li class="hornavbar"><a href="http://www.bike-design.com/nl/categorie/145020/bagage-shad" class="hornavlink" target="_blank">Bestel nu!</a></li>
-    <li class="hornavbar"><a href="./newsletter.php" class="hornavlink">Newsletter</a></li>
-    <li class="hornavbar"><a href="./contact__.php" class="hornavlink">Contact</a></li>
-  </ul>
   <div class="row">
     <div class="col-lg-12 col-sm-12 col-xs-12 col-md-12">
-      <nav id="myNavbar" class="navbar navbar-default" role="navigation">
+      <nav id="myNavbar" class="navbar navbar-inverse" role="navigation">
       <!-- Brand and toggle get grouped for better mobile display -->
       <div class="container">
         <div class="navbar-header">
@@ -27,14 +16,68 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Epse</a>
+          <a class="navbar-brand" href="#">Shad</a>
         </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="navbarCollapse">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="./about.html">About</a></li>
-            <li><a href="./contact.html">Contact</a></li>
+            <?php
+              if ($_SERVER['PHP_SELF'] == '/index.php') {
+                echo '<li class="active"><a href="#">Home</a></li>';
+              }
+              else {
+                echo '<li><a href="./index.php">Home</a></li>';
+              }
+            ?>
+            <?php
+              if ($_SERVER['PHP_SELF'] == '/welkom.php') {
+                echo '<li class="active"><a href="#">Home</a></li>';
+              }
+              else {
+                echo '<li><a href="./welkom.php">Welkom</a></li>';
+              }
+            ?>
+            <?php
+              if ($_SERVER['PHP_SELF'] == '/bienvenue.php') {
+                echo '<li class="active"><a href="#">Bienvenue</a></li>';
+              }
+              else {
+                echo '<li><a href="./bienvenue.php">Bienvenue</a></li>';
+              }
+            ?>
+            <?php
+              if ($_SERVER['PHP_SELF'] == '/products.php') {
+                echo '<li class="active"><a href="#">Producten</a></li>';
+              }
+              else {
+                echo '<li><a href="./products.php">Producten</a></li>';
+              }
+            ?>
+            <?php
+              if ($_SERVER['PHP_SELF'] == '/dealers.php') {
+                echo '<li class="active"><a href="#">Dealers</a></li>';
+              }
+              else {
+                echo '<li><a href="./dealers.php">Dealers</a></li>';
+              }
+            ?>
+            <li><a href="http://www.bike-design.com/nl/categorie/145020/bagage-shad" target="_blank">Bestel Nu!</a></li>
+            <?php
+              if ($_SERVER['PHP_SELF'] == '/index.php') {
+                echo '<li class="active"><a href="#">Nieuwsbrief</a></li>';
+              }
+              else {
+                echo '<li><a href="./newsletter.php">Nieuwsbrief</a></li>';
+              }
+            ?>
+            <?php
+              if ($_SERVER['PHP_SELF'] == '/contact__.php') {
+                echo '<li class="active"><a href="#">Contact</a></li>';
+              }
+              else {
+                echo '<li><a href="./contact__.php">Contact</a></li>';
+              }
+            ?>
           </ul>
         </div>
       </div>
