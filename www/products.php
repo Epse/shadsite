f403aaa326c4bfffccb23dd4eb28c499072e599f
Stef Pletinck <stef.plet@gmail.com>
2015-09-19 13:13:28 +0200

    fixed #6

--- www/products.php
+++ www/products.php
@@ -19,7 +19,31 @@
      <div class="col-lg-12">
        <div class="panel">
          <div class="panel-body">
-           <!-- Main content here -->
+           <div class="row">
+             <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+               <p>
+                 <b>Download de volledige <a href="http://shad.es/getCatalogue.php" target="_blank">Shad catalogus</a></b>
+                 <br>Voor een bestelling contacteer je best &eacute;&eacute;n van <a href="./dealers.php">de dealers</a>.
+               </p>
+               <p>
+                 Shad heeft een eigen verdeelcentrum in Belgi&euml; waardoor je geniet van een snelle levering en een onberispelijke dienst na verkoop.
+               </p>
+             </div>
+             <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+               <p>
+                 <b>Telechargez le <a href="http://shad.es/getCatalogue.php" target="_blank">catalogue de Shad</a> complet.</b>
+                 <br>Pour une commande, contactez un <a href="./dealers.php">des dealers</a> (vendeurs).
+               </p>
+               <p>
+                 SHAD &agrave; un centre de distribution en Belgique ce qui vous garantis d&#39;une livraison rapide et un service apr&egrave;s vente imp&eacute;cable.
+               </p>
+             </div>
+           </div>
+           <div class="row">
+             <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+               <img class="img-responsive center-block" src="./assets/images/shad_st38_aluminium.jpg" alt="Shad aluminium cases">
+             </div>
+           </div>
          </div>
        </div>
      </div>
