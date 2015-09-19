502a7e36e04d1368364bd97bb6ac4186adbc87a7
Stef Pletinck <stef.plet@gmail.com>
2015-09-19 14:13:13 +0200

    fixed #8

--- www/newsletter.php
+++ www/newsletter.php
@@ -12,47 +12,45 @@
 <!-- Latest compiled JavaScript -->
 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 </head>
-<bodys>
+<body>
  <div class="container">
    <!-- header include -->
    <?php include './include/header.php'; ?>
    <div class="row">
-     <div class="col-md-6 col-lg-6 col-sm-12 col-xs-12">
+     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
        <div class="panel">
          <div class="panel-body">
-          <p><br><b>Wil je graag op de&nbsp;hoogte blijven van de SHAD nieuwigheden en acties, schrijf je dan in op onze nieuwsbrief. </b><br><br>Het volstaat om een mail te sturen naar<a href="mailto:info@shad.be" target="_blank">info@shad.be</a> met de vraag om de nieuwsbrief te ontvangen. Je mailadres wordt bewaard en opgeslagen bij SHAD Belgi&euml; en niet doorgegeven aan externe partners. U wordt occasioneel op de hoogte gehouden van aanbiedingen en volgende activiteiten van Shad Belgi&euml;.</p>
-        </div>
-      </div>
-    </div>
-    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-      <div class="panel">
-        <div class="panel-body">
-          <p><br><b>Si vous souhaitez rester au courant de nous nouvauxt�s et actions, inscrivez-vous pour recevoir une lettre d&#8217;information par mail. </b><br><br>Cela suffit d&#8217;envoyer un mail &agrave; <a href="mailto:info@shad.be" target="_blank">info@shad.be</a> avec la demande de recevoir le lettre d&#8217;information. Votre adresse mail sera conserv&eacute; chez SHAD Belgique et ne sara pas distribu&eacute; &agrave; des tiers. On vous tiendra au courant de fa&ccedil;on occasionnel des nos offres et activit&eacute;s.</p>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="row">
-    <div class="panel">
-      <div class="panel-body">
-        <div class="col-lg-4 col-md-4 col-sm-12">
-          <img id="Picture2" height="185" width="200" src="./assets/images/shad_sh43_on_bike.jpg">
-        </div>
-        <div class="col-lg-4 col-md-4 col-sm-12">
-          <img id="Picture3" height="184" width="212" src="./assets/images/colour_plates.jpg">
-        </div>
-        <div class="col-lg-4 col-md-4 col-sm-12">
-          <img id="Picture4" height="184" width="240" src="./assets/images/SHAD-SH50.jpg">
-        </div>
-      </div>
-    </div>
-  </div>
-
-    <!-- footer include -->
-    <?php include 'include/footer.php'; ?>
-    </td>
-   </tr>
-  </table>
+           <div class="row">
+             <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+               <p>
+                 <b>Wil je graag op de hoogte blijven van de SHAD nieuwigheden en acties, schrijf je dan in op onze nieuwsbrief. </b>
+                 Het volstaat om een mail te sturen naar<a href="mailto:info@shad.be" target="_blank">info@shad.be</a> met de vraag om de nieuwsbrief te ontvangen. Je mailadres wordt bewaard en opgeslagen bij SHAD Belgi&euml; en niet doorgegeven aan externe partners. U wordt occasioneel op de hoogte gehouden van aanbiedingen en volgende activiteiten van Shad Belgi&euml;.
+               </p>
+             </div>
+             <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+               <p>
+                 <b>Si vous souhaitez rester au courant de nous nouvauxt&eacute;s et actions, inscrivez-vous pour recevoir une lettre d&#39;;information par mail. </b>
+                 Cela suffit d&#39;;envoyer un mail &agrave; <a href="mailto:info@shad.be" target="_blank">info@shad.be</a> avec la demande de recevoir le lettre d&#39;;information. Votre adresse mail sera conserv&eacute; chez SHAD Belgique et ne sara pas distribu&eacute; &agrave; des tiers. On vous tiendra au courant de fa&ccedil;on occasionnel des nos offres et activit&eacute;s.
+               </p>
+             </div>
+           </div>
+           <div class="row">
+             <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+               <img class="img-responsive" height="185" width="200" src="./assets/images/shad_sh43_on_bike.jpg">
+             </div>
+             <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+              <img class="img-responsive" height="184" width="212" src="./assets/images/colour_plates.jpg">
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+              <img class="img-responsive" height="184" width="240" src="./assets/images/SHAD-SH50.jpg">
+            </div>
+          </div>
+         </div>
+       </div>
+     </div>
+   </div>
+   <!-- footer include -->
+   <?php include 'include/footer.php'; ?>
   </div>
  </body>
  </html>
