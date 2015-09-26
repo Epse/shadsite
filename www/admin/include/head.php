211cc723558110315b57ca01dddd4e5a2d01cf8e
Stef Pletinck <stef.plet@gmail.com>
2015-09-26 16:20:04 +0200

    working

---
+++ www/admin/include/head.php
@@ -0,0 +1,15 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- Bootstrap - simplex theme -->
+<link href="../../assets/css/bootstrapshad.css" rel="stylesheet">
+<!-- jQuery library -->
+<script src="../../assets/js/jquery.js" type="text/javascript" charset="utf-8"></script>
+<!-- Latest compiled JavaScript -->
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<link rel="icon"
+  href="../../assets/favicon.ico">
+<!-- prettyPhoto stuffs -->
+<link rel="stylesheet" href="./assets/css/prettyPhoto.css" type="text/css" media="screen" charset="utf-8" />
+<script src="../../assets/js/jquery.prettyPhoto.js" type="text/javascript" charset="utf-8"></script>
+<script src="../../assets/js/jquery.rwdImageMaps.min.js"></script>
--- www/admin/includes/db_connect.php
+++
@@ -1,1 +0,0 @@
-<?php  $shad_backend_sql = new mysqli("localhost", "shad_php", "Rq1vbDY6BD", "shad_backend"); ?>
