09f632cba6b55c0714636b25f2f1058721803949
Stef Pletinck <stef.plet@gmail.com>
2015-09-26 11:37:35 +0200

    moved to singular head include file. You will see why

---
+++ www/include/head.php
@@ -0,0 +1,11 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- Bootstrap - simplex theme -->
+<link href="./bootstrapshad.css" rel="stylesheet">
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<!-- Latest compiled JavaScript -->
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<link rel="icon"
+  href="./assets/favicon.ico">
