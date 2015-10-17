69347271cd85fc9a0abd6425e29370100002940d
Epse <stef.plet@gmail.com>
2015-10-17 12:02:33 +0200

    added process script for the edit request. currently only uploads file.

---
+++ www/shadmin/processedit.php
@@ -0,0 +1,43 @@
+<?php
+
+$shad_backend_sql = new mysqli("localhost", "shad_php", "Rq1vbDY6BD", "shad_backend");
+$target_dir = "../assets/promoIMGS/";
+if (isset($_FILES['promoIMG']['tmp_name'])) {
+  $target_file = $target_dir . basename($_FILES['promoIMG']['name']);
+  $uploadOK = 1;
+  $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
+
+  // Check for real image
+    $check = getimagesize($_FILES['promoIMG']['tmp_name']);
+
+  if ($check) {
+    echo "File is an image - " . $check['mime'] . ".";
+    $uploadOK = 1;
+  } else {
+    echo "File is not an image.";
+    $uploadOK = 0;
+  }
+
+  if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOK = 0;
+  }
+
+  if ($imageFileType != "jpg" && $imageFileType != "png"
+    && $imageFileType != "jpeg" && $imageFileType != "gif") {
+      echo "Sorry, only JPG, JPEG, PNG & GIF allowed."
+      $uploadOK = 0;
+    }
+
+  if (!$uploadOK) {
+    echo "Sorry, try again.";
+  } else {
+    if (move_uploaded_file($_FILES['promoIMG']['tmp_name'], $target_file)) {
+      echo "The file " . basename($_FILES['promoIMG']['name']) . "has uploaded.";
+    } else {
+      echo 'An error occured.';
+    }
+  }
+}
+
+?>
