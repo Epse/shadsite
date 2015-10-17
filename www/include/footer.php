dfe82494ee71d880af8abf7d827d5e273600a955
Epse <stef.plet@gmail.com>
2015-10-17 17:12:52 +0200

    fixed some pagespeed things

--- www/include/footer.php
+++ www/include/footer.php
@@ -9,3 +9,9 @@
       <img src="./assets/images/logo-aniversario-shad-80.png" class="img-repsonsive center-block">
   </div>
 </div>
+<!-- jQuery library -->
+<script src="./assets/js/jquery.js" type="text/javascript" charset="utf-8"></script>
+<!-- Latest compiled JavaScript -->
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<script src="./assets/js/jquery.prettyPhoto.js" type="text/javascript" charset="utf-8"></script>
+<script src="./assets/js/jquery.rwdImageMaps.min.js"></script>
