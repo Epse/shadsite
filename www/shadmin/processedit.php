197eb683af19e824008b2ff297cdb71a7c1d2b6b
Epse <stef.plet@gmail.com>
2015-10-24 08:43:15 +0200

    the process pages didn't check for a correct login.... stupid me.

--- www/shadmin/processedit.php
+++ www/shadmin/processedit.php
@@ -1,61 +1,72 @@
 <?php
 
-$shad_backend_sql = new mysqli("localhost", "shad_php", "Rq1vbDY6BD", "shad_backend");
-$target_dir = "./assets/promoIMGS/";
-if (!empty($_FILES['promoIMG']['name'])) {
-  $target_file = $target_dir . basename($_FILES['promoIMG']['name']);
-  $uploadOK = 1;
-  $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
+// Define your username and password
+$username = "johan";
+$password = "Rn42DAay";
 
-  // Check for real image
-    $check = getimagesize($_FILES['promoIMG']['tmp_name']);
+if ($_COOKIE['shadmin'] == sha1($username . $password . $_SERVER['REMOTE_ADDR'])) {
 
-  if ($check) {
-    echo "File is an image - " . $check['mime'] . ".";
+  $shad_backend_sql = new mysqli("localhost", "shad_php", "Rq1vbDY6BD", "shad_backend");
+  $target_dir = "./assets/promoIMGS/";
+  if (!empty($_FILES['promoIMG']['name'])) {
+    $target_file = $target_dir . basename($_FILES['promoIMG']['name']);
     $uploadOK = 1;
-  } else {
-    echo "File is not an image.";
-    $uploadOK = 0;
-  }
+    $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
 
-  if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOK = 0;
-  }
+    // Check for real image
+      $check = getimagesize($_FILES['promoIMG']['tmp_name']);
 
-  if ($imageFileType != "jpg" && $imageFileType != "png"
-    && $imageFileType != "jpeg" && $imageFileType != "gif") {
-      echo "Sorry, only JPG, JPEG, PNG & GIF allowed.";
+    if ($check) {
+      echo "File is an image - " . $check['mime'] . ".";
+      $uploadOK = 1;
+    } else {
+      echo "File is not an image.";
       $uploadOK = 0;
     }
 
-  if (!$uploadOK) {
-    echo "Sorry, try again.";
-  } else {
-    if (move_uploaded_file($_FILES['promoIMG']['tmp_name'], "." . $target_file)) {
-      echo "The file " . basename($_FILES['promoIMG']['name']) . "has uploaded.";
+    if (file_exists($target_file)) {
+      echo "Sorry, file already exists.";
+      $uploadOK = 0;
+    }
+
+    if ($imageFileType != "jpg" && $imageFileType != "png"
+      && $imageFileType != "jpeg" && $imageFileType != "gif") {
+        echo "Sorry, only JPG, JPEG, PNG & GIF allowed.";
+        $uploadOK = 0;
+      }
+
+    if (!$uploadOK) {
+      echo "Sorry, try again.";
     } else {
-      echo 'An error occured.';
-      die();
+      if (move_uploaded_file($_FILES['promoIMG']['tmp_name'], "." . $target_file)) {
+        echo "The file " . basename($_FILES['promoIMG']['name']) . "has uploaded.";
+      } else {
+        echo 'An error occured.';
+        die();
+      }
     }
   }
-}
 
-// Now process other data
-$mysql_query = "UPDATE promos SET title='" . $_POST['txtTitle'] . "', active=";
+  // Now process other data
+  $mysql_query = "UPDATE promos SET title='" . $_POST['txtTitle'] . "', active=";
 
-if(!empty($_POST['chkActive'])) {
-  $mysql_query = $mysql_query . "1";
+  if(!empty($_POST['chkActive'])) {
+    $mysql_query = $mysql_query . "1";
+  } else {
+    $mysql_query = $mysql_query . "0";
+  }
+
+  if (!empty($uploadOK)) {
+    $mysql_query = $mysql_query . " ,html='" . $target_file . "'";
+  }
+
+  $mysql_query = $mysql_query . " WHERE creationDate='" . $_POST['creationDate'] . "'";
+
+  $shad_backend_sql->query($mysql_query);
+  echo '<a href="./index.php">Keer terug</a>'
+
 } else {
-  $mysql_query = $mysql_query . "0";
+  echo '<h3>Je bent niet ingelogd.</h3><br><a href="./index.php">Keer terug</a>';
 }
 
-if (!empty($uploadOK)) {
-  $mysql_query = $mysql_query . " ,html='" . $target_file . "'";
-}
-
-$mysql_query = $mysql_query . " WHERE creationDate='" . $_POST['creationDate'] . "'";
-
-$shad_backend_sql->query($mysql_query);
-echo '<a href="./index.php">Keer terug</a>'
 ?>
--- www/shadmin/processnew.php
+++ www/shadmin/processnew.php
@@ -1,64 +1,73 @@
 <?php
 
-$shad_backend_sql = new mysqli("localhost", "shad_php", "Rq1vbDY6BD", "shad_backend");
-$target_dir = "./assets/promoIMGS/";
-if (isset($_FILES['promoIMG']['tmp_name'])) {
-  $target_file = $target_dir . basename($_FILES['promoIMG']['name']);
-  $uploadOK = 1;
-  $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
+// Define your username and password
+$username = "johan";
+$password = "Rn42DAay";
 
-  // Check for real image
-    $check = getimagesize($_FILES['promoIMG']['tmp_name']);
-
-  if ($check) {
-    echo "File is an image - " . $check['mime'] . ".";
+if ($_COOKIE['shadmin'] == sha1($username . $password . $_SERVER['REMOTE_ADDR'])) {
+  $shad_backend_sql = new mysqli("localhost", "shad_php", "Rq1vbDY6BD", "shad_backend");
+  $target_dir = "./assets/promoIMGS/";
+  if (isset($_FILES['promoIMG']['tmp_name'])) {
+    $target_file = $target_dir . basename($_FILES['promoIMG']['name']);
     $uploadOK = 1;
-  } else {
-    echo "File is not an image.";
-    $uploadOK = 0;
-  }
+    $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
 
-  if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOK = 0;
-  }
+    // Check for real image
+      $check = getimagesize($_FILES['promoIMG']['tmp_name']);
 
-  if ($imageFileType != "jpg" && $imageFileType != "png"
-    && $imageFileType != "jpeg" && $imageFileType != "gif") {
-      echo "Sorry, only JPG, JPEG, PNG & GIF allowed.";
+    if ($check) {
+      echo "File is an image - " . $check['mime'] . ".";
+      $uploadOK = 1;
+    } else {
+      echo "File is not an image.";
       $uploadOK = 0;
     }
 
-  if (!$uploadOK) {
-    echo "Sorry, try again.";
-  } else {
-    if (move_uploaded_file($_FILES['promoIMG']['tmp_name'], "." . $target_file)) {
-      echo "The file " . basename($_FILES['promoIMG']['name']) . "has uploaded.";
+    if (file_exists($target_file)) {
+      echo "Sorry, file already exists.";
+      $uploadOK = 0;
+    }
 
-      // If unique is set, make all active promos inactive
-      if($_POST['chkUniq'] == 1) {
-        $shad_backend_sql->query("UPDATE promos SET active=0 WHERE active=1");
+    if ($imageFileType != "jpg" && $imageFileType != "png"
+      && $imageFileType != "jpeg" && $imageFileType != "gif") {
+        echo "Sorry, only JPG, JPEG, PNG & GIF allowed.";
+        $uploadOK = 0;
       }
 
-      if ($_POST['chkActiveNew'] == 1) {
-        $mysql_query = "INSERT INTO promos (title, active, html) VALUES ('"
-          . $_POST['txtTitleNew'] . "', 1, '"
-          . $target_file . "')";
-      } else {
-        $mysql_query = "INSERT INTO promos (title, active, html) VALUES ('"
-          . $_POST['txtTitleNew'] . "', 0, '"
-          . $target_file . "')";
-      }
-
-      $shad_backend_sql->query($mysql_query);
-
+    if (!$uploadOK) {
+      echo "Sorry, try again.";
     } else {
-      echo 'An error occured.';
-      die();
+      if (move_uploaded_file($_FILES['promoIMG']['tmp_name'], "." . $target_file)) {
+        echo "The file " . basename($_FILES['promoIMG']['name']) . "has uploaded.";
+
+        // If unique is set, make all active promos inactive
+        if($_POST['chkUniq'] == 1) {
+          $shad_backend_sql->query("UPDATE promos SET active=0 WHERE active=1");
+        }
+
+        if ($_POST['chkActiveNew'] == 1) {
+          $mysql_query = "INSERT INTO promos (title, active, html) VALUES ('"
+            . $_POST['txtTitleNew'] . "', 1, '"
+            . $target_file . "')";
+        } else {
+          $mysql_query = "INSERT INTO promos (title, active, html) VALUES ('"
+            . $_POST['txtTitleNew'] . "', 0, '"
+            . $target_file . "')";
+        }
+
+        $shad_backend_sql->query($mysql_query);
+
+      } else {
+        echo 'An error occured.';
+        die();
+      }
     }
   }
+
+  $shad_backend_sql->query($mysql_query);
+  echo '<a href="./index.php">Keer terug</a>';
+} else {
+  echo '<h3>Je bent niet ingelogd.</h3><br><a href="./index.php">Keer terug</a>';
 }
 
-$shad_backend_sql->query($mysql_query);
-echo '<a href="./index.php">Keer terug</a>';
 ?>
